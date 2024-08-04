d5f497f2c97c73f278c5b188ee51fb02d999c7ac
Makondelela <83129677+Makondelela@users.noreply.github.com>
2024-08-04 19:37:06 +0200

    Update EmailService.java

--- src/main/java/com/mako/authApp/service/EmailService.java
+++ src/main/java/com/mako/authApp/service/EmailService.java
@@ -29,7 +29,7 @@ public class EmailService {
                 "</html>";
 
 
-        helper.setFrom("makondelelamaps@gmail.com");
+        helper.setFrom("yourEmailAdress");
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
